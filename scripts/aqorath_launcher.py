7d098c29ae76ceac8af5381859a8a7ff93d98d96
kagenomusuko-svg <kage.no.musuko@gmail.com>
2026-09-12 22:18:17 -0600

    Make windowed Aqorath launcher safe without console streams

--- scripts/aqorath_launcher.py
+++ scripts/aqorath_launcher.py
@@ -1,6 +1,7 @@
 """Aqorath desktop application entrypoint."""
 
 import os
+import sys
 import threading
 import time
 import urllib.request
@@ -9,6 +10,14 @@ import webbrowser
 from aqorath.local_server import DEFAULT_PORT, LOCAL_HOST, run_local_surface
 
 LOCAL_URL = f"http://{LOCAL_HOST}:{DEFAULT_PORT}/"
+
+
+def _ensure_windowed_streams():
+    """Provide writable streams when PyInstaller --windowed omits the console."""
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, "w", encoding="utf-8")
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, "w", encoding="utf-8")
 
 
 def _open_browser_when_ready():
@@ -25,6 +34,8 @@ def _open_browser_when_ready():
 
 
 def main():
+    _ensure_windowed_streams()
+
     # CI sets this flag so the Windows smoke test does not launch a browser.
     if os.environ.get("AQORATH_NO_BROWSER") != "1":
         threading.Thread(
